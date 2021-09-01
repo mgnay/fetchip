249fded56161abdc9fb53b61a58429eb6a9d9cd5
mustafagunay <sayhimydude@gmail.com>
2021-09-01 21:21:51 +0300

    copy to clipboard

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/atotto/clipboard"
 	"net"
 	"net/http"
 	"os"
@@ -11,41 +12,51 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		var publicIP string
-		res, err := http.Get("http://ip.me/")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		defer res.Body.Close()
-		if res.StatusCode != http.StatusOK {
-			fmt.Println(err)
-		}
-
-		doc, err := goquery.NewDocumentFromReader(res.Body)
-		if res.StatusCode != http.StatusOK {
-			fmt.Println(err)
-		}
-
-		publicIP, _ = doc.Find("#ip-lookup").Attr("value")
-		fmt.Println("public ip:", publicIP)
+		getPublicIP()
 	} else {
-		host := os.Args[1]
-		host = strings.ReplaceAll(host, "http://", "")
-		host = strings.ReplaceAll(host, "https://", "")
-		if strings.Contains(host, "/") {
-			temp := strings.Index(host, "/")
-			host = host[:temp]
-		} else if strings.Contains(host, "\\") {
-			temp := strings.Index(host, "\\")
-			host = host[:temp]
-		}
-		ip, err := hostIP(host)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(ip)
-		}
+		getHostIP(os.Args[1])
+	}
+}
+
+func getPublicIP() {
+
+	var publicIP string
+	res, err := http.Get("http://ip.me/")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		fmt.Println(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if res.StatusCode != http.StatusOK {
+		fmt.Println(err)
+	}
+
+	publicIP, _ = doc.Find("#ip-lookup").Attr("value")
+	clipboard.WriteAll(publicIP)
+	fmt.Println("Your public IP: ", publicIP)
+}
+
+func getHostIP(host string) {
+	host = strings.ReplaceAll(host, "http://", "")
+	host = strings.ReplaceAll(host, "https://", "")
+	if strings.Contains(host, "/") {
+		temp := strings.Index(host, "/")
+		host = host[:temp]
+	} else if strings.Contains(host, "\\") {
+		temp := strings.Index(host, "\\")
+		host = host[:temp]
+	}
+	ip, err := hostIP(host)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		clipboard.WriteAll(fmt.Sprint(ip))
+		fmt.Println(host, " IPv4: ", ip)
 	}
 }
 
