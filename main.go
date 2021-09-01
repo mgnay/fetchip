25567f5035c31f68f935a6807b579ef0ed4ff682
mustafagunay <sayhimydude@gmail.com>
2021-09-01 20:55:40 +0300

    public IP

--- main.go
+++ main.go
@@ -2,29 +2,51 @@ package main
 
 import (
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
-	host:= os.Args[1]
-	host=strings.ReplaceAll(host, "http://", "")
-	host=strings.ReplaceAll(host, "https://", "")
-	if strings.Contains(host,"/"){
-		temp :=strings.Index(host,"/")
-		host =host[:temp]
-	}else if strings.Contains(host, "\\") {
-		temp:=strings.Index(host,"\\")
-		host =host[:temp]
-	}
-	ip,err:= hostIP(host)
-	if err != nil {
-		fmt.Println("Girdiğiniz adres hatalıdır...")
-	}else {
-		fmt.Println(ip)
-	}
+	if len(os.Args) == 1 {
+		var publicIP string
+		res, err := http.Get("http://ip.me/")
+		if err != nil {
+			fmt.Println(err)
+		}
 
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			fmt.Println(err)
+		}
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if res.StatusCode != http.StatusOK {
+			fmt.Println(err)
+		}
+
+		publicIP, _ = doc.Find("#ip-lookup").Attr("value")
+		fmt.Println("public ip:", publicIP)
+	} else {
+		host := os.Args[1]
+		host = strings.ReplaceAll(host, "http://", "")
+		host = strings.ReplaceAll(host, "https://", "")
+		if strings.Contains(host, "/") {
+			temp := strings.Index(host, "/")
+			host = host[:temp]
+		} else if strings.Contains(host, "\\") {
+			temp := strings.Index(host, "\\")
+			host = host[:temp]
+		}
+		ip, err := hostIP(host)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(ip)
+		}
+	}
 }
 
 func hostIP(host string) (net.IP, error) {
